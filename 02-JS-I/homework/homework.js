425daf986139e3b543cdb1977b70d713c31395a9
manuel bello <you@example.com>
2022-02-17 12:18:03 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,10 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = 'holoa';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = '7';
 
 // Crea una variable booleana:
 const nuevoBool = null;
@@ -23,7 +23,7 @@ const nuevoModulo = 21 % 5 === null;
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
 // Pista: "console.log()" NO fucionará.
-// No cambies los nombres de las funciones.
+
 
 function devolverString(str) {
   // "Return" la string provista: str
